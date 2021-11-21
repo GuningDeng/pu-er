149c8a2e77a337e60e463ac31d8718236b7cc2df
Deng Guning (s117661) <guning.deng@student.ap.be>
2021-11-21 17:54:07 +0100

    Finished React in Gatsby

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+        <Layout>
+            <p>
+                Pu erh tea, also written a pu’er, is a special type of tea that originates from China’s Yunnan province. 
+                This tea category is named after the city of puer. It was around a 1000 years ago when the people and horses carried lots of tea bricks and cakes from pu’er to nearby regions including Bengal, Burma, Tibet and Central China. This trade link is also called the ‘Ancient Tea Route’ or ‘Tea Horse Road’.
+            </p>
+
+        </Layout>
+    )
+}
+
+export default AboutPage
